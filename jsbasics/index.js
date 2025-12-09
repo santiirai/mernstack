1f877e1d8946ef3af0646a9e19851d9bfd57b345
shanti <shanti@gmail.com>
2025-12-09 12:17:37 +0545

    Hii

---
+++ jsbasics/index.js
@@ -0,0 +1,32 @@
+// // // // let a = 9;
+// // // // let b = 8;
+// // // // console.log(a+b);
+
+// // // const a = 8;
+// // // const b = 7;
+// // // console.log(a+b);
+
+
+// // //object
+// // let person = {
+// //     name: "Sita",
+// //     age: 20,
+// //     class : 6
+// // }
+
+// // console.log(person.name, "age is " + person.age);
+
+
+// // //array
+// // let persons = ["Shanti", 12,"BCA"]
+// // console.log(persons[1]);
+
+// let nameOfAnimal = ["Dog", "cat","Bear",null];
+// // console.log(nameOfAnimal[2,1], nameOfAnimal[2]);
+// for(let i= 0; i<=nameOfAnimal.length; i++){
+//     console.log(nameOfAnimal[i]);
+// }
+
+//template literals
+let name = "Ram";
+console.log(`My name is ${name}`);
